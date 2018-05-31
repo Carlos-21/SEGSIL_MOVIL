e48fb1e9767fccce642f333d36a81da272c00c95
Ricardo Morales <ricindigus@gmail.com>
2018-05-31 12:50:15 -0500

    pantallas splash y login

--- app/src/main/java/pe/edu/unmsm/sistemas/segsil/SplashActivity.java
+++ app/src/main/java/pe/edu/unmsm/sistemas/segsil/SplashActivity.java
@@ -1,14 +1,38 @@
 package pe.edu.unmsm.sistemas.segsil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class SplashActivity extends AppCompatActivity {
 
+    final int tiempoEspera = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null){
+            Intent intent = new Intent(getApplicationContext(),BienvenidoActivity.class);
+            startActivity(intent);
+            finish();
+        }else{
+            TimerTask timerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            };
+            Timer timer = new Timer();
+            timer.schedule(timerTask, tiempoEspera);
+        }
     }
 }
